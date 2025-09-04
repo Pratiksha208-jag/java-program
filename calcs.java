c8bff31a8db65096681471581fb46118d5e2b7c7
Pratiksha208-jag <pratiksha185jagdale@gmail.com>
2025-09-03 21:17:14 -0700

    Add files via upload

---
+++ calcs.java
@@ -0,0 +1,16 @@
+                                                                                                                                                                                                                          class calcs{
+ public static void main(String args[]){
+ int x,y;
+ x=10;y=15;
+sum=x+y;
+System.out.println("Addittion of x+y",sum);
+sub=x-y;
+System.out.println("Substraction of x-y",sub);
+mult=x*y;
+System.out.println("multiplication of x*y",mult);
+div=x/y;
+System.out.println("division of x/y",div);
+mod=x%y;
+System.out.println("modules of x%y",mod);
+}
+}                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                         
---
+++ swap.java
@@ -0,0 +1,10 @@
+class swap{
+public static void main(String args[]){
+ int a=10,b=20,t;
+t=a;
+a=b;
+b=t;
+System.out.println("after swapping a="+a);
+System.out.println("after swapping b="+b);
+}
+}
